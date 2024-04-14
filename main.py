3f16c4877e4802065bda588022cfef358515e00b
Emmanuel Momoh <102439386+nuel000@users.noreply.github.com>
2024-04-14 11:12:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
+import os
 import requests
-import sys
 
-def run():
-    url = "https://web.facebook.com/ads/library/?id=357741606925594"
-    splash_url = "http://splash:8050/render.html"
+url = "https://web.facebook.com/ads/library/?id=357741606925594"
+splash_url = os.getenv("SPLASH_URL")
 
-    params = {
-        'url': url,
-        'wait': 5
-    }
+params = {
+    'url': url,
+    'wait': 5
+}
 
-    r = requests.get(splash_url, params=params)
-    response_text = r.text
-
-    print(response_text)
-    sys.stdout.flush()
-run()
+r = requests.get(splash_url, params=params)
+print(r.text)
