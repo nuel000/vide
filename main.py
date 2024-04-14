f88b94dc31bce33eacef9bc9e4be805de863b7af
Emmanuel Momoh <102439386+nuel000@users.noreply.github.com>
2024-04-14 11:22:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import os
 import requests
-import sys
-
 
 url = "https://web.facebook.com/ads/library/?id=357741606925594"
 splash_url = os.getenv("SPLASH_URL")
 
 params = {
     'url': url,
-    'wait': 20
+    'wait': 5
 }
 
-r = requests.get(splash_url, params=params)
+# Define a custom session to capture requests
+session = requests.Session()
+
+# Define a custom hook function to capture requests
+def capture_requests(request, *args, **kwargs):
+    print("Captured URL:", request.url)
+    print("Request method:", request.method)
+    print("Request headers:", request.headers)
+    print("Request body:", request.body)
+
+# Register the hook function to the session
+session.hooks["response"] = [capture_requests]
+
+r = session.get(splash_url, params=params)
 print(r.text)
-sys.stdout.flush()
