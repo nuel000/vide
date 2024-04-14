86f9b79906e349d1ace8e06f5cf69f83018f94a0
Emmanuel Momoh <102439386+nuel000@users.noreply.github.com>
2024-04-14 11:17:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 import requests
+import sys
+
 
 url = "https://web.facebook.com/ads/library/?id=357741606925594"
 splash_url = os.getenv("SPLASH_URL")
 
 params = {
     'url': url,
-    'wait': 5
+    'wait': 20
 }
 
 r = requests.get(splash_url, params=params)
 print(r.text)
+sys.stdout.flush()
