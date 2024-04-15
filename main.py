d1035d7d65bee78f4208a8afba952da756d2b7da
Emmanuel Momoh <102439386+nuel000@users.noreply.github.com>
2024-04-15 23:09:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-
-from playwright.sync_api import Playwright, sync_playwright, expect
-from bs4 import BeautifulSoup
+from scrapingbee import ScrapingBeeClient
 import sys
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://twitter.com/chainlink")
-    html = page.content()
-    s = BeautifulSoup(html,'html.parser')
-    print(s.text)
-    sys.stdout.flush()
+client = ScrapingBeeClient(api_key='NP63V99OC92MT0LQWVGA56GEH32NEZ183YIHJ6XNPBCDSMTCH06ZI38QYB1G5I47UUP44H4S141W0VA2')
 
-    context.close()
-    browser.close()
+response = client.get("https://web.facebook.com/ads/library/?active_status=all&ad_type=political_and_issue_ads&country=NG&id=1054028289359210&media_type=all")
 
-with sync_playwright() as playwright:
-    run(playwright)
+print('Response HTTP Status Code: ', response.status_code)
+sys.stdout.flush()
+print('Response HTTP Response Body: ', response.content)
+sys.stdout.flush()
